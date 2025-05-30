ae19625c60b1eb7ff0221773c82783aeb07a21c0
FARHAN-VAI-MIRAY <vaifarhan768@gmail.com>
2025-05-30 17:25:06 +0600

    Update speech.js

--- scripts/commands/speech.js
+++ scripts/commands/speech.js
@@ -1,93 +1,50 @@
+const fs = require("fs");
 module.exports = {
-
   config:{
-
-     name: "speech",
-
-     version: "1.0.0",
-
-     permission: 0,
-
-     credits: "NAYAN",
-
-     prefix: 'awto',
-
-     description: "text to voice",
-
-     category: "user",
-
-     usages: "text",
-
-     cooldowns: 0,
-
-     dependencies: {
-
-       "fs": "",
-
-       "axios": "",
-
-       "nayan-server": "^2.3.6"
-
-     },
-
-    envConfig: {
-
-      name: 'Nabanita'
-
-    }
-
+	name: "npx3",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "😒",
+        cooldowns: 5, 
 },
 
-  languages: {
+handleEvent: async function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+  const axios = require('axios')
+  const NAYAN = ["https://i.imgur.com/LLucP15.mp4",
+		 "https://i.imgur.com/DEBRSER.mp4",
+		"https://i.imgur.com/8jJJcfA.mp4",
+                "https://i.imgur.com/hc8Ed6F.mp4",
+                "https://i.imgur.com/9OlKrSQ.mp4",
+                "https://i.imgur.com/rPOCiNG.mp4",
+                "https://i.imgur.com/LMx3GRL.mp4",
+                "https://i.imgur.com/EpSBt3r.mp4",
+                "https://i.imgur.com/1iaf4A8.mp4",
+                "https://i.imgur.com/36e6ZGT.mp4"
+		]
+    var rndm = NAYAN[Math.floor(Math.random() * NAYAN.length)];
+const media = (
+    await axios.get(
+      `${rndm}`,
+      { responseType: 'stream' }
+    )
+  ).data;
 
-  "vi": {},
-
-      "en": {
-
-        "error": 'âŒ Something Went WrongðŸ±'
-
-      }
-
-  },
-
-start: async function ({ nayan, args, events, lang }) {
-
-  try {
-
-    const { text2voice } = require('nayan-server');
-
-    const axios = require('axios');
-
-    const fs = require('fs');
-
-    var content = (events.type == "message_reply") ? events.messageReply.body : args.join(" ");
-
-    const name = 'Nabanita'
-
-    const d = await text2voice(content, global.configModule[this.config.name].name);
-
-    console.log(d)
-
-    var audioss = []
-
-    const voice = d.url;
-
-    var { data } = await axios.get(voice, { method: 'GET', responseType: 'arraybuffer' });
-
-    fs.writeFileSync(__dirname + "/cache/text2voice.m4a", Buffer.from(data, 'utf-8'));
-
-    audioss.push(fs.createReadStream(__dirname + "/cache/text2voice.m4a"));
-
-    var msg = { body: content, attachment: audioss }
-
-    nayan.reply(msg, events.threadID, events.messageID)
-
-  } catch (error) {
-
-    nayan.reply(lang("error"), events.threadID, events.messageID)
-
+	if (body.indexOf("FARHAN")==0 || body.indexOf("farhan")==0 || body.indexOf("Farhan")==0 || body.indexOf("ফারহান")==0 || body.indexOf("🤧")==0 || body.indexOf("🥵")==0 || body.indexOf("😎")==0 || body.indexOf("🤒")==0 || body.indexOf("😴")==0 || body.indexOf("😪")==0) {
+		var msg = {
+				body: "🖤🥀",
+				attachment: media
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
   }
-
-}
-
-} 
+  }
